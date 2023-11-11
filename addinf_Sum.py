e258ce1011aa76e3e893d38ea80cfb152c44d0c9
syedzubyl <syedzubyl96@gmail.com>
2023-11-11 08:11:45 +0530

    syed

---
+++ addinf_Sum.py
@@ -0,0 +1,18 @@
+def arr_sum(arr, size):
+    if size == 0:
+        return 0
+    else:
+        return arr[size - 1] + arr_sum(arr, size - 1)
+
+n = int(input("Enter the number for array size: "))
+a = []
+
+for i in range(0, n):
+    ele = int(input("Enter the element one by one: "))
+    a.append(ele)
+
+print("The given array is:")
+print(a)
+
+print("The total sum of the array elements is:", arr_sum(a, n))
+
