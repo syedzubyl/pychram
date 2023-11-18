b840b79e2cbce2d404c68756d4acf5315741f3cc
syedzubyl <syedzubyl96@gmail.com>
2023-11-18 23:13:59 +0530

    syed

--- stack.py
+++ stack.py
@@ -1,19 +1,52 @@
+# Initial empty STACK
 stack = []
-stackSize = 5
+stacksize = 5
+
+# Display Menu with Choices
 while True:
-    print("The STACK ADT OPERATION")
-    print("----------------------- \n")
-    print("\n SELECT APPROPRIATE CHOICE")
-    print("1. PUSH Element into the stack")
-    print("2. pop the element into the stack")
-    print("3. Display the element in the stack")
-    print("4.Exit")
-    print(" ")
+    print("")
+    print("STACK ADT OPERATION")
+    print("-----------------------------------\n")
+    print("\nSELECT APPROPRIATE CHOICE")
+    print("1. PUSH Element into the Stack")
+    print("2. POP Element from the Stack")
+    print("3. Display Elements of the Stack")
+    print("4. Exit")
+    print("")
+    
     choice = int(input("Enter the Choice:"))
+    
+    # push
     if choice == 1:
-        if len(stack) == stackSize:
+        if len(stack) == stacksize:
             print("Stack is full")
         else:
-            n = int(input("Enter the len into stack"))
-            for i in range(0,n):
-                ele=int(input("enter the element to push on to stack"))
+            n = int(input("Enter the number of elements for the list:"))
+            for i in range(0, n):
+                element = input("Enter the element to push onto the stack:")
+                stack.append(element)
+            print('\nTotal elements PUSHED into the STACK', i + 1)
+    
+    # pop
+    elif choice == 2:
+        if len(stack) == 0:
+            print('The STACK is EMPTY. No element to POP out')
+        else:
+            print('\nElement POPPED out from the STACK is:', stack.pop())
+    
+    # display the STACK
+    elif choice == 3:
+        if len(stack) == 0:
+            print('The STACK is initially EMPTY')
+        else:
+            print("The Size of the STACK is: ", len(stack))
+            print('\nSTACK elements are as follows:')
+            print(stack)
+    
+    # EXIT from the program
+    elif choice == 4:
+        break
+    
+    # Shows ERROR message if the choice is not in between 1 to 4
+    else:
+        print("Incorrect Choice")
